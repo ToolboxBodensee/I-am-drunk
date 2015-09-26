a79ce75c2e58d11052baf64f871b622f89814255
Jonas Otto <jottosmail@gmail.com>
2015-09-26 14:43:47 +0200

    Added material design

--- Iamdrunk/app/src/main/java/org/toolboxbodensee/iamdrunk/MainActivity.java
+++ Iamdrunk/app/src/main/java/org/toolboxbodensee/iamdrunk/MainActivity.java
@@ -1,12 +1,13 @@
 package org.toolboxbodensee.iamdrunk;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
