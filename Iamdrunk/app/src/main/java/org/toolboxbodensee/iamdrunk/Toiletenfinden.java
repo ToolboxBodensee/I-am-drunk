0667bc8a1d8e85fddb7549865a1c5b976193d8ee
Paul Nykiel <paul.nykiel@gmail.com>
2015-09-26 12:34:48 +0200

    Read file from toilet db

--- Iamdrunk/app/src/main/java/org/toolboxbodensee/iamdrunk/Toiletenfinden.java
+++ Iamdrunk/app/src/main/java/org/toolboxbodensee/iamdrunk/Toiletenfinden.java
@@ -3,6 +3,10 @@ package org.toolboxbodensee.iamdrunk;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class Toiletenfinden extends ActionBarActivity {
 
     @Override
@@ -11,4 +15,22 @@ public class Toiletenfinden extends ActionBarActivity {
         setContentView(R.layout.activity_toiletenfinden);
     }
 
+    private void loadDB(){
+        try {
+            URL url = new URL("http://www.gratispinkeln.de/loadmarkers.php");
+            Scanner s = new Scanner(url.openStream());
+            // read from your scanner
+
+            String text  = s.toString();
+
+            
+
+        }
+        catch(IOException ex) {
+            // there was some connection problem, or the file did not exist on the server,
+            // or your URL was not in the right format.
+            // think about what to do now, and put it here.
+            ex.printStackTrace(); // for now, simply output it.
+        }
+    }
 }
