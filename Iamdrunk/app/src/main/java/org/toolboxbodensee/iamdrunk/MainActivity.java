00a85d688ac582bc1e9e88e9477ae5e73a3b005b
Paul Nykiel <paul.nykiel@gmail.com>
2015-09-26 14:23:14 +0200

    Read file from toilet db async, progress bar

--- Iamdrunk/app/src/main/java/org/toolboxbodensee/iamdrunk/MainActivity.java
+++ Iamdrunk/app/src/main/java/org/toolboxbodensee/iamdrunk/MainActivity.java
@@ -1,9 +1,11 @@
 package org.toolboxbodensee.iamdrunk;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -35,5 +37,10 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void toiletteTest(View view){
+        Intent intent = new Intent(this, Toiletenfinden.class);
+        startActivity(intent);
     }
 }
--- Iamdrunk/app/src/main/java/org/toolboxbodensee/iamdrunk/Toiletenfinden.java
+++ Iamdrunk/app/src/main/java/org/toolboxbodensee/iamdrunk/Toiletenfinden.java
@@ -1,9 +1,13 @@
 package org.toolboxbodensee.iamdrunk;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
@@ -13,24 +17,51 @@ public class Toiletenfinden extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toiletenfinden);
+
+        loadDB();
     }
 
     private void loadDB(){
+        URL url = null;
         try {
-            URL url = new URL("http://www.gratispinkeln.de/loadmarkers.php");
-            Scanner s = new Scanner(url.openStream());
-            // read from your scanner
-
-            String text  = s.toString();
-
-            
-
+            url = new URL("http://www.gratispinkeln.de/loadmarkers.php");
+            new DownloadDBTask().execute(url);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
         }
-        catch(IOException ex) {
-            // there was some connection problem, or the file did not exist on the server,
-            // or your URL was not in the right format.
-            // think about what to do now, and put it here.
-            ex.printStackTrace(); // for now, simply output it.
+
+    }
+
+    class DownloadDBTask extends AsyncTask<URL, Integer, String> {
+
+        private Exception exception;
+
+        @Override
+        protected String doInBackground(URL... url) {
+            try {
+                publishProgress((int)0);
+                Scanner s = new Scanner(url[0].openStream());
+                String text = "";
+                publishProgress(2);
+                while(s.hasNextLine())
+                    text += s.nextLine();
+                publishProgress((int)100);
+                return text;
+            } catch (Exception e) {
+                this.exception = e;
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String text) {
+            TextView textView = (TextView)findViewById(R.id.testedit);
+            textView.setText(text);
+        }
+
+        protected void onProgressUpdate(Integer... progress) {
+            ProgressBar progressBar = (ProgressBar)findViewById(R.id.progressLoad);
+            progressBar.setProgress(progress[0]);
         }
     }
 }
