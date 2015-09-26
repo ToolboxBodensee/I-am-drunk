20b3351de3614080dbf200fd66bbe19b5eead611
Jonas Otto <jottosmail@gmail.com>
2015-09-26 17:10:28 +0200

    now extends Activity

--- Iamdrunk/app/src/main/java/org/toolboxbodensee/iamdrunk/PromilleRechnerActivity.java
+++ Iamdrunk/app/src/main/java/org/toolboxbodensee/iamdrunk/PromilleRechnerActivity.java
@@ -1,5 +1,6 @@
 package org.toolboxbodensee.iamdrunk;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,7 +12,7 @@ import android.widget.Switch;
 import android.widget.TextView;
 
 
-public class PromilleRechnerActivity extends ActionBarActivity {
+public class PromilleRechnerActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
