2647ebebd04e0d447d3e208dcd62dd57decb6d27
Tovermodus <simon@mabisile.de>
2015-09-26 16:39:58 +0200

    Promillerechner completed

--- Iamdrunk/app/src/main/java/org/toolboxbodensee/iamdrunk/PromilleRechnerActivity.java
+++ Iamdrunk/app/src/main/java/org/toolboxbodensee/iamdrunk/PromilleRechnerActivity.java
@@ -4,6 +4,11 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 
 
 public class PromilleRechnerActivity extends ActionBarActivity {
@@ -12,6 +17,42 @@ public class PromilleRechnerActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_promille_rechner);
+        Button berechnen = (Button)findViewById(R.id.promille_berechnen);
+        berechnen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView ergebnis = (TextView)findViewById(R.id.promille_ergebnis);
+                Switch geschlecht = (Switch)findViewById(R.id.geschlecht);
+                EditText korpergewicht = (EditText)findViewById(R.id.korpergewicht_input);
+                EditText zeit = (EditText)findViewById(R.id.zeit_input);
+                EditText e [] [] = new EditText[3][5];
+                e[0][0] = (EditText)findViewById(R.id.alkoholgehalt_input);
+                e[0][1] = (EditText)findViewById(R.id.alkoholgehalt_input2);
+                e[0][2] = (EditText)findViewById(R.id.alkoholgehalt_input3);
+                e[0][3] = (EditText)findViewById(R.id.alkoholgehalt_input4);
+                e[0][4] = (EditText)findViewById(R.id.alkoholgehalt_input5);
+                e[1][0] = (EditText)findViewById(R.id.menge_input);
+                e[1][1] = (EditText)findViewById(R.id.menge_input2);
+                e[1][2] = (EditText)findViewById(R.id.menge_input3);
+                e[1][3] = (EditText)findViewById(R.id.menge_input4);
+                e[1][4] = (EditText)findViewById(R.id.menge_input5);
+                e[2][0] = (EditText)findViewById(R.id.anzahl_input);
+                e[2][1] = (EditText)findViewById(R.id.anzahl_input2);
+                e[2][2] = (EditText)findViewById(R.id.anzahl_input3);
+                e[2][3] = (EditText)findViewById(R.id.anzahl_input4);
+                e[2][4] = (EditText)findViewById(R.id.anzahl_input5);
+                double alkoholmenge = 0;
+                for(int i = 0; i < 5; i++)
+                    alkoholmenge += Double.parseDouble(e[0][i].getText().toString())*Double.parseDouble(e[1][i].getText().toString())* Integer.parseInt(e[2][i].getText().toString())*10;   //Blutalkohol(ml)
+                double promille_ergebnis = 0;
+                if(geschlecht.isChecked())
+                    promille_ergebnis = alkoholmenge/(Double.parseDouble(korpergewicht.getText().toString())*0.55) - Double.parseDouble(zeit.getText().toString())*0.15;
+                else
+                    promille_ergebnis = alkoholmenge/(Double.parseDouble(korpergewicht.getText().toString())*0.68) - Double.parseDouble(zeit.getText().toString())*0.15;
+
+                    ergebnis.setText(promille_ergebnis+"");
+            }
+        });
     }
 
 
