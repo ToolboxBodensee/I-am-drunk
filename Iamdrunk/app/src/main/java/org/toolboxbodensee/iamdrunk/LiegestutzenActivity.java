cb1ead2e65c2f9d4d0dee37b0b3cc58dde5a6a22
Paul Nykiel <paul.nykiel@gmail.com>
2015-09-26 22:26:38 +0200

    Liegestutzen fertig

--- Iamdrunk/app/src/main/java/org/toolboxbodensee/iamdrunk/LiegestutzenActivity.java
+++ Iamdrunk/app/src/main/java/org/toolboxbodensee/iamdrunk/LiegestutzenActivity.java
@@ -1,15 +1,40 @@
 package org.toolboxbodensee.iamdrunk;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class LiegestutzenActivity extends ActionBarActivity {
+public class LiegestutzenActivity extends Activity {
+    Button btn;
+    int counter=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liegestutzen);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        btn = (Button)findViewById(R.id.btnLiegestutz);
+    }
+
+    public void buttonPress(View view){
+
+        btn.setText(Integer.toString(counter++)+" Liegestützen geschafft!");
+    }
+
+    public void fertigPress(View view){
+        SharedPreferences settings = getSharedPreferences("LiegestutzenConfig", 0);
+        int limit = settings.getInt("anzahl", 10);
+        Button btnFertig = (Button)findViewById(R.id.btnFertig);
+        if(counter<limit)
+            btnFertig.setText("Du bist betrunken!");
+        else
+            btnFertig.setText("Du bist noch nüchtern!");
+
+        btn.setText("Mach soviele Liegestützen wie du kannst und versuche dabei jedes mal mit der Nase den Bilschirm zu berühren!");
+        counter=0;
+
     }
 
 }
