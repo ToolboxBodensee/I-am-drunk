6a08c30bcd6b0f934fcdf79bc651f6883313de1d
Tovermodus <simon@mabisile.de>
2015-09-27 02:20:15 +0200

    Reaktionstest

--- Iamdrunk/app/src/main/java/org/toolboxbodensee/iamdrunk/PunktAntippenMinispielActivity.java
+++ Iamdrunk/app/src/main/java/org/toolboxbodensee/iamdrunk/PunktAntippenMinispielActivity.java
@@ -1,25 +1,33 @@
 package org.toolboxbodensee.iamdrunk;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class PunktAntippenMinispielActivity extends ActionBarActivity {
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class PunktAntippenMinispielActivity extends Activity {
+
+    PunktDrawView drawView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_punkt_antippen_minispiel);
+        drawView = new PunktDrawView(this);
+        setContentView(drawView);
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_punkt_antippen_minispiel, menu);
         return true;
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -29,9 +37,11 @@ public class PunktAntippenMinispielActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            drawView.invalidate();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 }
+
---
+++ Iamdrunk/app/src/main/java/org/toolboxbodensee/iamdrunk/PunktDrawView.java
@@ -0,0 +1,110 @@
+package org.toolboxbodensee.iamdrunk;
+
+/**
+ * Created by simon on 26.09.15.
+ */
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class PunktDrawView extends View implements View.OnTouchListener{
+    Paint paint = new Paint();
+    double speed = 0.8;
+    double counter = 0;
+
+    double a2x = 0,  a2y = 0;
+    double ax = 0,  ay = 0;
+    double vx = (Math.random() - 0.5) * speed * 5,  vy = (Math.random() - 0.5) * speed * 5;
+    double x = 100, y = 100;
+    double timer = 10;
+    double score = 0;
+    public PunktDrawView(Context context) {
+        super(context);
+        paint.setColor(Color.BLACK);
+        this.setOnTouchListener(this);
+    }
+    public void move()
+    {
+        if(Math.random()< 0.2)
+        ax += (Math.random() - 0.5) * 5* speed; ay += (Math.random() - 0.5) * 5 * speed;
+        vx += ax; vy += ay;
+        if(vx > speed * 80) {
+            vx /= 2;
+            ax /= -2;
+        }
+        if(vx < speed * -80) {
+            vx /= 2;
+            ax /= -2;
+        }
+        if(vy > speed * 80) {
+            vy /= 2;
+            ay /= -2;
+        }
+        if(vy < speed * -80) {
+            vy /= 2;
+            ay /= -2;
+        }
+        x += vx; y += vy;
+        timer -= 0.1 + counter;
+        score += 0.1 + counter;
+        if(x > this.getWidth()-150)
+            x = -50;
+        else if(x < -50)
+            x = this.getWidth()-150;
+        if(y > this.getHeight()-150)
+            y = -50;
+        else if(y < -50)
+            y = this.getHeight()-1501 ;
+        counter += 0.001;
+    }
+    @Override
+    public void onDraw(Canvas canvas) {
+        canvas.drawBitmap(BitmapFactory.decodeResource(this.getContext().getResources(),
+                R.drawable.punkt19302087), (float)x, (float)y, paint);
+        paint.setTextSize(36);
+        timer*=100;
+        timer = Math.round(timer);
+        timer/=100;
+        score*=100;
+        score = Math.round(score);
+        score/=100;
+        canvas.drawText("Zeit verbleibend:" + timer, 100, 100, paint);
+        canvas.drawText("Score:" + score, 100,200, paint);
+        if(timer>0)
+            move();
+        else {
+            paint.setTextSize(72);
+            if(score < 25)
+            {
+                canvas.drawText("Du bist zu betrunken" + timer, 100, 300, paint);
+            }
+            else
+                canvas.drawText("Du bist noch nüchtern!", 50,300,paint );
+            timer = 0;
+        }
+        try {
+            Thread.sleep(30);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        invalidate();
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        if(timer >0 && event.getAction() == MotionEvent.ACTION_DOWN)
+        {
+            if((event.getX()- (x + 100))*(event.getX()-( x + 100)) + (event.getY()-(y + 100))*(event.getY()-(y + 100)) < 13000)
+                timer += 4;
+            Log.e("hjgkig", (event.getX()- (x + 100))*(event.getX()-( x + 100)) + (event.getY()-(y + 100))*(event.getY()-(y + 100)) + "");
+
+        }
+        return true;
+    }
+}
