c6c7964bc5d705abb265462dec7f283d15bfcee7
Tovermodus <simon@mabisile.de>
2015-09-27 05:03:04 +0200

    Bild Beschreiben link gefunden

--- Iamdrunk/app/src/main/java/org/toolboxbodensee/iamdrunk/DescribeImageActivity.java
+++ Iamdrunk/app/src/main/java/org/toolboxbodensee/iamdrunk/DescribeImageActivity.java
@@ -1,17 +1,16 @@
 package org.toolboxbodensee.iamdrunk;
 
 import android.app.Activity;
-import android.app.DownloadManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
-import android.webkit.WebView;
-import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -29,7 +28,9 @@ public class DescribeImageActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_describe_image);
-        client= new OkHttpClient();
+        client = new OkHttpClient();
+
+        new Connection().execute();
     }
 
 
@@ -62,5 +63,49 @@ public class DescribeImageActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+    private class Connection extends AsyncTask {
+        String data;
+        @Override
+        protected Object doInBackground(Object[] params) {
+            Log.e("jfjhg", "aaaa");
+            try {
+                data = run("https://api.imgur.com/3/gallery/random/random/1");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+            @Override
+            protected void onPostExecute (Object o){
+                super.onPostExecute(o);
+                Log.e("jfjhg", "ssss");
+                JSONArray jsonArray = null;
+                JSONObject jsonObject = null;
+                Log.e("jfjhg", "dddd");
+                try {
+                    Log.e("jfjhg", "ffff");
+                    jsonObject = new JSONObject(data);
+                    jsonArray = jsonObject.getJSONArray("data");
+                    Log.e("jfjhg", jsonArray.toString());
+                    Log.e("jfjhg", "gggg");
+                } catch (JSONException e) {
+                    Log.e("jfjhg", "hhhh");
+                    e.printStackTrace();
+                }
+
+                Log.e("jfjhg", "jjjj");
+                Log.e("jfjhg", jsonArray.length() + "");
+                int zielindex = (int) (Math.random() * jsonArray.length());
+                try {
+                    Log.e("jfjhg", jsonArray.getJSONObject(zielindex).get("link").toString());
+                    ImageView img = (ImageView)findViewById(R.id.desc_image);
+                    //img.setImageBitmap(jsonArray.getJSONObject(zielindex).get("link").toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
 }
 
