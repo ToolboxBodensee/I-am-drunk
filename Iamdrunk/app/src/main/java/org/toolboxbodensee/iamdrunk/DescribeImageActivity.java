bbcb2073ed28bacda76aa6fe232f6f2edcd05829
Paul Nykiel <paul.nykiel@gmail.com>
2015-09-27 06:46:06 +0200

    Clarifai in Bild Suche

--- Iamdrunk/app/src/main/java/org/toolboxbodensee/iamdrunk/DescribeImageActivity.java
+++ Iamdrunk/app/src/main/java/org/toolboxbodensee/iamdrunk/DescribeImageActivity.java
@@ -1,6 +1,7 @@
 package org.toolboxbodensee.iamdrunk;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
@@ -8,7 +9,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
@@ -18,23 +23,37 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
+
+import com.clarifai.api.ClarifaiClient;
+import com.clarifai.api.RecognitionRequest;
+import com.clarifai.api.RecognitionResult;
+import com.clarifai.api.Tag;
+import com.clarifai.api.exception.ClarifaiException;
 
 
 public class DescribeImageActivity extends Activity {
-
+    private static final String APP_ID = "0oyGmUkNF8s6Ha399dnpf8KqzPun7OW9aMTSHTyf";
+    private static final String APP_SECRET = "tD2Ujs_Gi5NRfK9Oyp52dmTWRhPqHlEm3A_r86we";
 
     String link ="";
     OkHttpClient client;
+
+    List<RecognitionResult> results;
+
+    EditText input;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_describe_image);
         client = new OkHttpClient();
-
+        input = (EditText)findViewById(R.id.desc_input);
         new LinkConnection().execute();
     }
 
@@ -52,6 +71,31 @@ public class DescribeImageActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_describe_image, menu);
         return true;
+    }
+
+    public void enterPress(View view){
+        boolean success = false;
+        TextView txt = (TextView)findViewById(R.id.desc_label);
+
+        for (Tag tag : results.get(0).getTags()) {
+            if(input.getText().toString().contains(tag.getName())) 
+                success = true;
+
+        }
+        input.setText("");
+        if (success) {
+            txt.setText("Du bist noch nüchtern!");
+            launchAppChooser();
+        }
+        else
+            txt.setText("Du bist betrunken!");
+    }
+
+    private void launchAppChooser() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 
 
@@ -102,25 +146,31 @@ public class DescribeImageActivity extends Activity {
         @Override
         protected void onPostExecute (Object o){
             super.onPostExecute(o);
-            JSONArray jsonArray = null;
-            JSONObject jsonObject = null;
             try {
-                jsonObject = new JSONObject(data);
-                jsonArray = jsonObject.getJSONArray("data");
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
+                JSONArray jsonArray = null;
+                JSONObject jsonObject = null;
+                try {
+                    jsonObject = new JSONObject(data);
+                    jsonArray = jsonObject.getJSONArray("data");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
 
-            int zielindex = (int) (Math.random() * jsonArray.length());
-            try {
-                link = jsonArray.getJSONObject(zielindex).get("link").toString();
+                int zielindex = (int) (Math.random() * jsonArray.length());
+                try {
+                    link = jsonArray.getJSONObject(zielindex).get("link").toString();
+                    new BMPConnection().execute();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
                 new BMPConnection().execute();
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            new BMPConnection().execute();
+            } catch (NullPointerException e) {
+                    e.printStackTrace();
+                }
+
         }
-    }private class BMPConnection extends AsyncTask {
+    }
+    private class BMPConnection extends AsyncTask {
         Bitmap bmp;
         @Override
         protected Object doInBackground(Object[] params) {
@@ -131,9 +181,29 @@ public class DescribeImageActivity extends Activity {
         @Override
         protected void onPostExecute (Object o){
             super.onPostExecute(o);
+            ImageView img = (ImageView)findViewById(R.id.desc_image);
+            img.setImageBitmap(bmp);
+            new Clarifai().execute();
 
-                ImageView img = (ImageView)findViewById(R.id.desc_image);
-                img.setImageBitmap(bmp);
+        }
+    }
+    private class Clarifai extends AsyncTask {
+        @Override
+        protected Object doInBackground(Object[] params) {
+            ClarifaiClient clarifai = new ClarifaiClient(APP_ID, APP_SECRET);
+            results = clarifai.recognize(new RecognitionRequest(link));
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute (Object o){
+            super.onPostExecute(o);
+
+
+
+
         }
     }
 
